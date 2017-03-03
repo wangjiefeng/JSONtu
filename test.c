2fd5e2caf7a7909e4f8deaaaa0702c7939a33e91
wangjiefeng <1414059957@qq.com>
2017-03-03 12:46:53 +0800

    commit

--- test.c
+++ test.c
@@ -7,6 +7,8 @@ static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
+//注意宏的书写和多行语句后的“\” 使用do-while(0) 以下语句都可以执行一遍
+//注意有两个编译器选项：__FILE__指的是可执行文件名 __LINE__支出行数
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
@@ -18,8 +20,10 @@ static int test_pass = 0;
         }\
     } while(0)
 
+//进一步简化宏调用
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
+//测试解析NULL
 static void test_parse_null() {
     lept_value v;
     v.type = LEPT_FALSE;
@@ -27,6 +31,21 @@ static void test_parse_null() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
+static void test_parse_true() {
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+	EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+
+static void test_parse_false() {
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+	EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
+}
+
+//测试解析
 static void test_parse_expect_value() {
     lept_value v;
 
@@ -39,6 +58,7 @@ static void test_parse_expect_value() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
+//测试解析非法值
 static void test_parse_invalid_value() {
     lept_value v;
     v.type = LEPT_FALSE;
@@ -59,6 +79,8 @@ static void test_parse_root_not_singular() {
 
 static void test_parse() {
     test_parse_null();
+	test_parse_true();
+	test_parse_false();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
