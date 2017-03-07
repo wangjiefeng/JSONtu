a28655dc3101d096cd0d016afa3533e66cbfc0fb
wangjiefeng <1414059957@qq.com>
2017-03-07 23:09:06 +0800

    add parse number

--- leptjson.c
+++ leptjson.c
@@ -42,13 +42,24 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
 	return LEPT_PARSE_OK;
 }
 
+static int lept_parse_number(lept_context* c, lept_value* v) {
+    char* end;
+    v->n = strtod(c->json, &end);
+    if (c->json == end)
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json = end;
+    v->type = LEPT_NUMBER;
+    return LEPT_PARSE_OK;
+
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
 		case 't':  return lept_parse_true(c, v);
 		case 'f':  return lept_parse_false(c, v);
-        case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
+		default:   return lept_parse_number(c, v);
+		case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
 }
 
@@ -70,4 +81,8 @@ int lept_parse(lept_value* v, const char* json) {
 lept_type lept_get_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
+}
+double lept_get_number(lept_value* v) {
+	assert(v != NULL && v->type == LEPT_NUMBER);
+	return v->n;
 }
--- leptjson.h
+++ leptjson.h
@@ -2,8 +2,9 @@
 #define LEPTJSON_H__
 
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
-
+//1.增加数据成员double n；
 typedef struct {
+	double n;
     lept_type type;
 }lept_value;
 
@@ -17,5 +18,7 @@ enum {
 int lept_parse(lept_value* v, const char* json);
 
 lept_type lept_get_type(const lept_value* v);
+
+double lept_get_number(lept_value* v);
 
 #endif /* LEPTJSON_H__ */
